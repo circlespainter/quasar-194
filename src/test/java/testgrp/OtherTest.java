202025b43e8007f51562bb424a381dc0ef94c293
circlespainter <fabio@paralleluniverse.co>
2016-06-13 18:52:46 +0300

    Use Quasar 0.7.5 and the automatic suspendables scanner

--- src/test/java/testgrp/OtherTest.java
+++ src/test/java/testgrp/OtherTest.java
@@ -2,24 +2,35 @@ package testgrp;
 
 import co.paralleluniverse.fibers.FiberUtil;
 import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.fibers.Suspendable;
 import co.paralleluniverse.strands.Strand;
-import co.paralleluniverse.strands.SuspendableRunnable;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
 
 public class OtherTest {
-    private static synchronized void syncM() throws InterruptedException, SuspendExecution {
-        Strand.sleep(100);
+    interface I {
+        void doSusp();
+    }
+
+    private static synchronized void syncM(I i) {
+        i.doSusp();
     }
 
     @Test
     public void testSync() throws ExecutionException, InterruptedException {
-        FiberUtil.runInFiber(new SuspendableRunnable() {
+        FiberUtil.runInFiber(() -> syncM(new I() {
             @Override
-            public void run() throws SuspendExecution, InterruptedException {
-                syncM();
+            @Suspendable
+            public void doSusp() {
+                try {
+                    Strand.sleep(100);
+                } catch (final SuspendExecution e) {
+                    throw new AssertionError(e);
+                } catch (final InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        });
+        }));
     }
 }
